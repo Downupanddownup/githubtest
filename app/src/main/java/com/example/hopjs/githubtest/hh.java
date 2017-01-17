7e7113c62223b484f9a23432aa6626b1b5e93863
费风 <pingdengjia0liu@163.com>
2017-01-17 20:34:32 +0800

    版本三

---
+++ app/src/main/java/com/example/hopjs/githubtest/hh.java
@@ -0,0 +1,8 @@
+package com.example.hopjs.githubtest;
+
+/**
+ * Created by Hopjs on 2017/1/17.
+ */
+
+public class hh {
+}
---
+++ app/src/main/java/com/example/hopjs/githubtest/uu.java
@@ -0,0 +1,9 @@
+package com.example.hopjs.githubtest;
+
+/**
+ * Created by Hopjs on 2017/1/17.
+ */
+
+public class uu {
+    String a="1";
+}
